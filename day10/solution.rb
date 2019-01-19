cacef58c5f2277c80653fa9f1687fe6cb446ed93
Jonathan Lalande <lalandej.gg@gmail.com>
2019-01-19 00:43:02 -0500

    part 1

--- day10/solution.rb
+++ day10/solution.rb
@@ -1,12 +1,28 @@
 #!/usr/bin/env ruby
 
-def part1
-  input = File.read('input.txt')
+# Reverse the order of that length of elements in the list, starting with the element at the current position.
+# Move the current position forward by that length plus the skip size.
+# Increase the skip size by one.
+
+input = File.read('input.txt')
+list = (0...256).to_a
+
+current = 0
+skip = 0
+lengths = input.split(',').map(&:to_i)
+
+lengths.each do |n|
+  # update current position
+  list.rotate!(current)
+
+  list = list.shift(n).reverse + list
+
+  list.rotate!(-current)
+
+  current += n + skip
+
+  # increate skip
+  skip += 1
 end
 
-def part2
-  input = File.read('input.txt')
-end
-
-puts "Solution part1: #{part1}"
-puts "Solution part2: #{part2}"
+p list.take(2).reduce(:*)
